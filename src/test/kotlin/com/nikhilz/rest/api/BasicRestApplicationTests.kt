8e1254c5cb5c7227d95f471f7b6f446b445e2c4f
Nikhil Gopi <nikhi.gopi@cinesoft.live>
2023-10-25 14:16:45 +0530

    Initial commit

---
+++ src/test/kotlin/com/nikhilz/rest/api/BasicRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nikhilz.rest.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicRestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
